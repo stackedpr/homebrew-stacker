fd4508c91bce53630193cb70e6ec01722b532d56
Yair Haimovitch <yairh@wix.com>
2021-04-18 21:45:52 +0300

    update formula urls

--- Formula/stacker.rb
+++ Formula/stacker.rb
@@ -1,7 +1,7 @@
 class Stacker < Formula
   desc "Create Stacked Pull Requests"
-  homepage "https://stacker-site.yairhaimo.now.sh/"
-  url "https://homebrew-stacker.yairhaimo.now.sh/stacker-v0.0.6.tar.gz"
+  homepage "https://stacker-site-yairhaimo.vercel.app/"
+  url "https://homebrew-stacker-yairhaimo.vercel.app/stacker-v0.0.6.tar.gz"
   version "0.0.6"
   sha256 "929d47c2778a4a4fc1d0f4d4f0d2db0196782e1b97a190b77a5e555cffe4994e"
   depends_on "hub"
