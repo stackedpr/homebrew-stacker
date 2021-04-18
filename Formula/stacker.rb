70ae37c39680ec162fe1868f4afcb842689a30b7
Yair Haimovitch <yairh@wix.com>
2021-04-18 22:15:20 +0300

    v0.0.8

--- Formula/stacker.rb
+++ Formula/stacker.rb
@@ -1,9 +1,9 @@
 class Stacker < Formula
   desc "Create Stacked Pull Requests"
   homepage "https://stacker-site-yairhaimo.vercel.app/"
-  url "https://homebrew-stacker-yairhaimo.vercel.app/stacker-v0.0.7.tar.gz"
-  version "0.0.7"
-  sha256 "989bcfaf41fdc8fac9551315c879ea6fe6e498e25e42a4c5a8baaf1fb8ab5f73"
+  url "https://homebrew-stacker-yairhaimo.vercel.app/stacker-v0.0.8.tar.gz"
+  version "0.0.8"
+  sha256 "2822652569c43d1c92487ecce8fc47d7c4e89f6269146b4437b59dffa48256d0"
   depends_on "hub"
 
   def install
@@ -11,6 +11,6 @@ class Stacker < Formula
   end
 
   test do
-    assert_match "Stacker v0.0.7", shell_output("#{bin}/stacker --version", 2)
+    assert_match "Stacker v0.0.8", shell_output("#{bin}/stacker --version", 2)
   end
 end
